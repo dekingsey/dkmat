26f58d6e9698124ffac06c19b6806d76dcb38f6f
Tommy Prevost <git@tomix.ca>
2025-03-08 19:55:48 -0500

    modification de dkmat.py: - pour ajouter la documentation - pour ajouter jolie_fraction qui affiche les racines carrées avec l'opérateur √ - pour appeler jolie_fraction dans pprint() modification de test_dkmat.py pour tester les nouvelles fonctions.

--- dkmat.py
+++ dkmat.py
@@ -3,17 +3,27 @@ import numpy.linalg as la
 from fractions import Fraction
 
 def asvec(*args):
+  """
+  Convertit les paramètres reçus en un vecteur.
+  :param args: Tous les nombres composant le vecteur.
+  :return Un vecteur sous forme matricielle, c'est à dire une matrice Nx1. 
+  """
   v=asmat(args, 1)
   return(v.T)
 
 def asmat(*args, **kwargs):
-  # peut être appelé ainsi:
-  #  - asmat([1,2,3,4]) retournera une matrice 2x2 (on utiliser la racine carrée pour déterminer le nombre de rangées)
-  #  - asmat((1,2,3,4,5,6), 2) retournera une matrice 2x3
-  #  - asmat(1,2,3,4,5,6, r=2) retournera une matrice 2x3
-  #  - asmat(1,2,3,4) retournera une matrice 2x2 (on utiliser la racine carrée pour déterminer le nombre de rangées)
-  # r = nombre de rangées (si à 0, détecté par la racine carrée)
-  # l = liste à traiter
+  """
+  Convertit les paramètres reçus en une matrice.
+
+  Exemples d'utilisation:
+   - asmat([1,2,3,4]) retournera une matrice 2x2 (on utiliser la racine carrée pour déterminer le nombre de rangées)
+   - asmat((1,2,3,4,5,6), 2) retournera une matrice 2x3
+   - asmat(1,2,3,4,5,6, r=2) retournera une matrice 2x3
+   - asmat(1,2,3,4) retournera une matrice 2x2 (on utiliser la racine carrée pour déterminer le nombre de rangées)
+  :param args: Tous les nombres composants le vecteur ou encore une énumération suivi d'une nombre de lignes.
+  :param kwargs: 'r' peut être spécifié pour forcer un nombre de lignes. Sinon, on aura une matrice carrée avec un nombre de colonnes plus grand que le nombre de rangées avec un nombre de rangées le plus près possible de la racine carrée.
+  :return Une matrice formée des nombres reçus.
+  """
   r = 0
 
   if "r" in kwargs:
@@ -52,7 +62,40 @@ def asmat(*args, **kwargs):
 
   return(arr)
 
+
+def jolie_fraction(n):
+  """
+  Transforme un nombre en format fractionnel. Si les termes de la fraction sont des racines carrées, ils seront affichés avec l'opérateur √.
+  :param n: Nombre à transformer.
+  :return Une chaîne de caractères x/y.
+  """
+  LIMITE = 10E3
+  f = Fraction(n).limit_denominator()
+  if f.numerator < LIMITE and f.denominator < LIMITE:
+    return str(f)
+  else:
+    f2 = Fraction(pow(n, 2)).limit_denominator()
+    if f2.numerator < LIMITE and f2.denominator < LIMITE:
+      retval = []
+      for i in f2.as_integer_ratio():
+        carre = pow(i,.5)
+        if carre - int(carre) < 1/LIMITE:
+          # si on est au denominateur, retval existe
+          # si le denominateur est 1, on ignore le denominateur
+          if retval and carre != 1:
+            retval.append(str(int(carre)))
+        else:
+          retval.append(f"√{i}")
+      return "/".join(retval)
+    else:
+      return str(f)
+
+
 def pprint(A):
+  """
+  Affiche sous forme d'un tableau une matrice reçue en paramètre. Les nombres rationnels seront affichés sous forme de fraction. Les racines carrées seront affichées avec l'opérateur √.
+  :param A: Matrice à afficher.
+  """
   if isinstance(A, np.ndarray) and A.ndim == 2:
     l = []
     maxlen = 0
@@ -60,7 +103,7 @@ def pprint(A):
       rl = []
       l.append(rl)
       for v in r:
-        sv = str(Fraction(v).limit_denominator())
+        sv = jolie_fraction(v)
         rl.append(sv)
         maxlen = max(len(sv), maxlen)
     for rl in l:
@@ -73,32 +116,37 @@ def pprint(A):
 
 
 def gauss_jordan_rref(A):
-    # Copie de la matrice pour ne pas modifier l'originale
-    A = A.astype(float)  # Utiliser un type float pour éviter les erreurs lors de la division
-    rows, cols = A.shape
-    row = 0
+  """
+  Réduit une matrice sous sa forme échelonnée.
+  :param A: Matrice à réduire.
+  :return: Matrice réduite.
+  """
+  # Copie de la matrice pour ne pas modifier l'originale
+  A = A.astype(float)  # Utiliser un type float pour éviter les erreurs lors de la division
+  rows, cols = A.shape
+  row = 0
 
-    for col in range(cols):
-        # Chercher la ligne avec un pivot non nul
-        if np.any(A[row: , col] != 0):  # Vérifier s'il existe un pivot non nul dans la colonne
-            # Chercher la ligne avec un pivot non nul dans cette colonne (pivot au dessus de la ligne actuelle)
-            for i in range(row, rows):
-                if A[i, col] != 0:
-                    # Echanger les lignes pour amener le pivot à la position de la ligne actuelle
-                    A[[row, i]] = A[[i, row]]
-                    break
+  for col in range(cols):
+    # Chercher la ligne avec un pivot non nul
+    if np.any(A[row: , col] != 0):  # Vérifier s'il existe un pivot non nul dans la colonne
+      # Chercher la ligne avec un pivot non nul dans cette colonne (pivot au dessus de la ligne actuelle)
+      for i in range(row, rows):
+        if A[i, col] != 0:
+          # Echanger les lignes pour amener le pivot à la position de la ligne actuelle
+          A[[row, i]] = A[[i, row]]
+          break
 
-            # Normaliser la ligne du pivot
-            A[row] = A[row] / A[row, col]
-            
-            # Annuler les éléments sous et au-dessus du pivot
-            for i in range(rows):
-                if i != row:
-                    A[i] = A[i] - A[i, col] * A[row]
-            row += 1
+      # Normaliser la ligne du pivot
+      A[row] = A[row] / A[row, col]
+      
+      # Annuler les éléments sous et au-dessus du pivot
+      for i in range(rows):
+        if i != row:
+          A[i] = A[i] - A[i, col] * A[row]
+      row += 1
 
-        # Si tous les pivots sont trouvés, on arrête la réduction
-        if row == rows:
-            break
+    # Si tous les pivots sont trouvés, on arrête la réduction
+    if row == rows:
+      break
 
-    return A
+  return A
--- test_dkmat.py
+++ test_dkmat.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 from dkmat import *
 
+
 #################
 # asvec
 def test_conversion_vecteur_avec_liste():
@@ -10,11 +11,13 @@ def test_conversion_vecteur_avec_liste():
   # Vérification que la matrice résultante est correcte
   assert np.array_equal(result, expected)
 
+
 def test_conversion_vecteur():
   result = asvec(0,1,2,3,4,5,6)
   expected = np.array([[0], [1], [2], [3], [4], [5], [6]])
   # Vérification que la matrice résultante est correcte
   assert np.array_equal(result, expected)
+
 
 #################
 # asmat
@@ -24,11 +27,13 @@ def test_conversion_matrice_parametre_r_et_tuple():
   # Vérification que la matrice résultante est correcte
   assert np.array_equal(result, expected)
 
+
 def test_conversion_matrice_2x2_avec_2_tuples():
   result = asmat((0,1),(0,1))
   expected = np.array([[0,1], [0,1]])
   # Vérification que la matrice résultante est correcte
   assert np.array_equal(result, expected)
+
 
 def test_conversion_matrice_2x2_avec_args():
   result = asmat(0,1,2,3)
@@ -36,30 +41,36 @@ def test_conversion_matrice_2x2_avec_args():
   # Vérification que la matrice résultante est correcte
   assert np.array_equal(result, expected)
 
+
 def test_conversion_matrice_2x2():
   result = asmat(range(4))
   expected = np.array([[0, 1], [2, 3]])
   # Vérification que la matrice résultante est correcte
   assert np.array_equal(result, expected)
 
+
 def test_conversion_matrice_2x3_avec_args():
   result = asmat(0,1,2,3,4,5,r=2)
   expected = np.array([[0, 1, 2], [3, 4, 5]])
   assert np.array_equal(result, expected)
+
 
 def test_conversion_matrice_2x3():
   result = asmat(range(6), 2)
   expected = np.array([[0, 1, 2], [3, 4, 5]])
   assert np.array_equal(result, expected)
 
+
 def test_conversion_matrice_3x3():
   result = asmat(range(9))
   expected = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
   assert np.array_equal(result, expected)
 
+
 def test_exception_conversion_incorrecte():
   with pytest.raises(ValueError):
     asmat(range(7), 2)
+
 
 def test_conversion_matrice_1x1():
   result = asmat([42])
@@ -74,6 +85,21 @@ def test_pprint(capsys):
   expected_output = " 0   1/11 \n2/11 3/11 \n"
   assert captured.out == expected_output
 
+
+def test_pprint_sqrt_fraction(capsys):
+  pprint(asvec([1 / pow(i,.5) for i in range(1,5)]))
+  captured = capsys.readouterr()
+  expected_output = " 1  \n √2 \n √3 \n1/2 \n"
+  assert captured.out == expected_output
+
+
+def test_pprint_sqrt(capsys):
+  pprint(asvec([pow(i,.5) for i in range(1,5)]))
+  captured = capsys.readouterr()
+  expected_output = "1  \n√2 \n√3 \n2  \n"
+  assert captured.out == expected_output
+
+
 #################
 # rref
 def test_rref():
@@ -81,10 +107,12 @@ def test_rref():
   expected = np.eye(2)
   assert np.array_equal(expected, result)
 
+
 def test_rref_3x3_avec_nulle():
   result = gauss_jordan_rref(asmat((1,2,3,1,2,3,1,2,4)))
   expected = asmat((1,2,0,0,0,1,0,0,0))
   assert np.array_equal(expected, result)
+
 
 def test_rref_2x3():
   result = gauss_jordan_rref(asmat(range(6),2))
